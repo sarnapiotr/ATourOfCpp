d48f16c40d3450eb4c4f59c092451bf3a1850a87
Piotr Sarna <95252277+sarnapiotr@users.noreply.github.com>
2025-04-28 13:18:31 +0200

    Update 1. The Basics.cpp

--- 1. The Basics/1. The Basics.cpp
+++ 1. The Basics/1. The Basics.cpp
@@ -11,6 +11,20 @@ void assignments() {
 	auto d = 1.2;
 }
 
+void constants() {
+	const int dmv = 17;
+	int var = 17;
+
+	constexpr double max1 = dmv;//OK, dmv is a constant expression
+	//constexpr double max2 = var; error: var is not a constant expression
+	const double max3 = var;//OK, may be avaluated at run time
+	//The same with functions
+}
+
+void pointersArraysReferences() {
+
+}
+
 int main()
 {
 	return 0;
